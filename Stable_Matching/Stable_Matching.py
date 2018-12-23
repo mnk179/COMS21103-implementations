9c8899cc1d79dca746be402db2e065549a06221d
mnk179 <martynas.nor@gmail.com>
2018-12-23 19:58:11 +0000

    Add protections against adding non Match preferences

--- Stable_Matching/Stable_Matching.py
+++ Stable_Matching/Stable_Matching.py
@@ -3,12 +3,20 @@ def stableMatching(proposers, proposees):
         proposers: list of entities that submit proposals
         proposees: list of entities that consider the proposals from the proposers
     """
-    pass
+    for proposer in proposers:
+        pass
+
 
 class Match(object):
+    """
+        name: string
+        free: boolean
+        preferences: list of Match objects, in order of preference
+    """
     def __init__(self, name):
         self.name = name
         self.free = True
+        self._preferences = []
 
     # Getters and setters are not Pythonic
 
@@ -17,3 +25,14 @@ class Match(object):
 
     def __repr__(self):
         return self.__str__()
+
+    @property
+    def preferences(self):
+        return self._preferences
+
+    @preferences.setter
+    def preferences(self, values):
+        if not all(isinstance(value, Match) for value in values):
+            raise TypeError("Preferences must be of type Match")
+        else:
+            self._preferences = values
--- Stable_Matching/tests.py
+++ Stable_Matching/tests.py
@@ -2,12 +2,28 @@ import unittest
 from Stable_Matching import *
 
 class TestMatchClass(unittest.TestCase):
+
     def testCreateMatchObject(self):
         johnDoe = Match("John Doe")
+
     def testStrAndRepr(self):
+        janeDoe = Match("Jane Doe")
+        str(janeDoe)
+        repr(janeDoe)
+
+    def testSetPreferences(self):
         johnDoe = Match("John Doe")
-        str(johnDoe)
-        repr(johnDoe)
+        janeDoe = Match("Jane Doe")
+        adamSmith = Match("Adam Smith")
+        adamSmith.preferences = [janeDoe, johnDoe]
+        # Check if preferences set
+        self.assertEqual(adamSmith.preferences, [janeDoe, johnDoe])
+
+    def testSetPreferencesInvalid(self):
+        adamSmith = Match("Adam Smith")
+        with self.assertRaises(TypeError):
+            adamSmith.preferences = ["John Doe", "Jane Doe"]
+
 
 if __name__ == "__main__":
     unittest.main()
