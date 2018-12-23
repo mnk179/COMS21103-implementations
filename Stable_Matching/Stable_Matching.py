7bdf2a28376b5f6a65acdf00fa99398f171a8c67
mnk179 <martynas.nor@gmail.com>
2018-12-23 21:53:21 +0000

    Finish Stable_Matching and add test cases

--- Stable_Matching/Stable_Matching.py
+++ Stable_Matching/Stable_Matching.py
@@ -1,11 +1,35 @@
-def stableMatching(proposers, proposees):
-    """
-        proposers: list of entities that submit proposals
-        proposees: list of entities that consider the proposals from the proposers
-    """
-    for proposer in proposers:
-        pass
+from collections import deque
 
+def stableMatching(proposors, acceptors):
+    """
+        proposors: list of entities that submit proposals
+        acceptors: list of entities that consider the proposals from the proposors
+    """
+    deq = deque(proposors)
+    while deq:
+        proposor = deq.popleft()
+        for preference in proposor.preferences:
+            if preference.free:
+                proposor.pair = preference
+                preference.pair = proposor
+                preference.free = False
+                proposor.free = False
+                break
+            elif not preference.free:
+                preferencePair = preference.pair
+                preferencePairIndex = preference.preferences.index(preference.pair)
+                proposorIndex = preference.preferences.index(proposor)
+                if proposorIndex < preferencePairIndex:
+                    preferencePair.free = True
+                    preferencePair.pair = None
+                    deq.appendleft(preferencePair)
+                    proposor.pair = preference
+                    preference.pair = proposor
+                    preference.free = False
+                    proposor.free = False
+                    break
+            else:
+                raise ValueError("Unexpected status")
 
 class Match(object):
     """
@@ -17,11 +41,12 @@ class Match(object):
         self.name = name
         self.free = True
         self._preferences = []
+        self.pair = None
 
     # Getters and setters are not Pythonic
 
     def __str__(self):
-        return "A Match object with name " + self.name
+        return self.name
 
     def __repr__(self):
         return self.__str__()
--- Stable_Matching/tests.py
+++ Stable_Matching/tests.py
@@ -24,6 +24,86 @@ class TestMatchClass(unittest.TestCase):
         with self.assertRaises(TypeError):
             adamSmith.preferences = ["John Doe", "Jane Doe"]
 
+    def testEmployersPreference(self):
+        # Initialise employers
+        veolia = Match("Veolia")
+        weibo = Match("Weibo")
+        xerox = Match("Xerox")
+        yahoo = Match("Yahoo")
+        zurich = Match("Zurich")
+
+        employers = [veolia, weibo, xerox, yahoo, zurich]
+
+        # Initialise students
+        abby = Match("Abby")
+        ben = Match("Ben")
+        callum = Match("Callum")
+        davida = Match("Davida")
+        erika = Match("Erika")
+
+        students = [abby, ben, callum, davida, erika]
+
+        # Set employers' preference lists
+        veolia.preferences = [ben, abby, davida, erika, callum]
+        weibo.preferences = [davida, ben, abby, callum, erika]
+        xerox.preferences = [ben, erika, callum, davida, abby]
+        yahoo.preferences = [abby, davida, callum, ben, erika]
+        zurich.preferences = [ben, davida, abby, erika, callum]
+
+        # Set students' preference lists
+        abby.preferences = [zurich, veolia, weibo, yahoo, xerox]
+        ben.preferences = [xerox, weibo, yahoo, veolia, zurich]
+        callum.preferences = [weibo, xerox, yahoo, zurich, veolia]
+        davida.preferences = [veolia, zurich, yahoo, xerox, weibo]
+        erika.preferences = [yahoo, weibo, zurich, xerox, veolia]
+
+        stableMatching(employers, students)
+
+        employersResult = [emp.pair for emp in employers]
+        employersTestAgainst = [abby, callum, ben, erika, davida]
+
+        self.assertEqual(employersResult, employersTestAgainst)
+
+        studentsResult = [stu.pair for stu in students]
+        studentsTestAgainst = [veolia, xerox, weibo, zurich, yahoo]
+
+        self.assertEqual(studentsResult, studentsTestAgainst)
+
+    def testProposorsAcceptors(self):
+        # https://en.wikipedia.org/wiki/Stable_marriage_problem#/media/File:Gale-Shapley.gif
+
+        # Initialise proposors
+        p = [Match(1), Match(2), Match(3), Match(4)]
+
+        # Initialise acceptors
+        a = [Match(1), Match(2), Match(3), Match(4)]
+
+        # Set proposors' preferences
+        p[0].preferences = [a[1], a[0], a[2], a[3]]
+        p[1].preferences = [a[3], a[0], a[1], a[2]]
+        p[2].preferences = [a[0], a[2], a[1], a[3]]
+        p[3].preferences = [a[1], a[2], a[0], a[3]]
+
+        # Set acceptors' preferences
+        a[0].preferences = [p[0], p[2], p[1], p[3]]
+        a[1].preferences = [p[2], p[3], p[0], p[1]]
+        a[2].preferences = [p[3], p[1], p[2], p[0]]
+        a[3].preferences = [p[2], p[1], p[0], p[3]]
+
+        stableMatching(p, a)
+
+        proposorsResult = [el.pair for el in p]
+        proposorsTestAgainst = [a[0], a[3], a[2], a[1]]
+
+        self.assertEqual(proposorsResult, proposorsTestAgainst)
+
+        acceptorsResult = [el.pair for el in a]
+        acceptorsTestAgainst = [p[0], p[3], p[2], p[1]]
+
+        self.assertEqual(acceptorsResult, acceptorsTestAgainst)
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
